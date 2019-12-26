98d766218eb98d8523c6e9f5facb534f42f276e7
Misha <Lampard0307@yandex.ru>
2019-12-26 23:38:04 +0300

    fixed tests

--- test/test.js
+++ test/test.js
@@ -1,8 +1,12 @@
+import * as fetchMock from "sinon";
+
+const sinon = require('sinon');
 const mocha = require('mocha');
 const chai = require('chai');
 const {show, showError, search} = require('../js/logic');
 const {JSDOM} = require('jsdom');
 const {spy} = require('sinon');
+const expect = chai.expect;
 Handlebars = require('../libs/handlebars-v4.4.3');
 
 let window;
@@ -35,6 +39,24 @@ describe('Test functions', () => {
         });
     });
 
+    it('Document weather render called correctly', () => {
+        JSDOM.fromFile("index.html", {runScripts: "dangerously", resources: "usable"}).then(dom => {
+            fetchMock.mock('*', mockSuccessResponse);
+            let callback = sinon.spy();
+            PubSub.subscribe("weatherRender", callback);
+            global.Handlebars = Handlebars;
+            global.document = dom.window.document;
+            global.weatherFetch = sinon.stub();
+            app.__get__('inputCallback')({
+                preventDefault: () => {
+                }, target: [{value: 'Moscow'}]
+            });
+            PubSub.publishSync("weatherRender");
+            assert.equal(callback.called, true);
+        });
+        fetchMock.reset();
+    });
+
     it('show', () => {
         show.call(window, data);
         assert.include(window.document.getElementsByClassName("container")[0].innerHTML,
@@ -47,34 +69,6 @@ describe('Test functions', () => {
             '<li><span>Скорость ветра</span> <b>' + data.wind.speed + ' м/c</b></li>');
         assert.include(window.document.getElementsByClassName("container")[0].innerHTML,
             '<li><span>Описание</span> <b>' + data.weather[0].description + '</b></li>');
-
-        /*
-        it('show temperature', () => {
-            show.call(window, data);
-            assert.include(window.document.getElementsByClassName("container")[0].innerHTML,
-                '<li><span>Температура</span> <b>' + data.main.temp + ' °С</b></li>');
-        });
-        it('show pressure', () => {
-            show.call(window, data);
-            assert.include(window.document.getElementsByClassName("container")[0].innerHTML,
-                '<li><span>Давление</span> <b>' + data.main.pressure + ' гПа</b></li>');
-        });
-        it('show humidity', () => {
-            show.call(window, data);
-            assert.include(window.document.getElementsByClassName("container")[0].innerHTML,
-                '<li><span>Влажность</span> <b>' + data.main.humidity + ' %</b></li>');
-        });
-        it('show wind speed', () => {
-            show.call(window, data);
-            assert.include(window.document.getElementsByClassName("container")[0].innerHTML,
-                '<li><span>Скорость ветра</span> <b>' + data.wind.speed + ' м/c</b></li>');
-        });
-        it('show description', () => {
-            show.call(window, data);
-            assert.include(window.document.getElementsByClassName("container")[0].innerHTML,
-                '<li><span>Описание</span> <b>' + data.weather[0].description + '</b></li>');
-        });
-        */
     });
 
     describe('showError', () => {
@@ -97,11 +91,32 @@ describe('Test functions', () => {
         });
     });
 
-    it('search', () => {
-        let city = 'Murmansk';
-        let result = search.call(window, city);
-        console.log(result);
-        assert.include(result.q,
-            city);
+    describe('search', () => {
+        it('response', async () => {
+            let pseudoAxios = {
+                get: () => {
+                    return Promise.resolve({
+                        data: data
+                    });
+                }
+            };
+            let curData = await search.call(window, "", pseudoAxios);
+            assert.include(curData, data);
+        });
+
+        it('error', async () => {
+            let pseudoAxios = {
+                get: () => {
+                    return Promise.reject({
+                        response: {
+                            status: 400
+                        }
+                    });
+                }
+            };
+            let curData = await search.call(window, "", pseudoAxios);
+            console.log(curData.status);
+            assert.equal(curData.status, '400');
+        });
     });
 });
